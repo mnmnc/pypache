9df35bef85127ba7364c8613c37d0426d13eed76
mnmnc <chooseright+git@gmail.com>
2015-02-12 16:20:37 +0100

    Fixxxing. Abandonning regex.

--- pypache.py
+++ pypache.py
@@ -3,41 +3,44 @@
 # IMPORTS
 import re
 import socket
-from colorama import init, Fore, Back, Style
+
  
 # VARS
-regex = '([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)"'
-log_path = '/var/log/apache2/access.log'
+log_path = 'apache.log'
 previous_ip = " "
 previous_host = " "
  
 # FUNCTIONS
 def adjust_item( str, i ):
-    if len(str) < i:
-        for j in range(i-len(str)):
-            str = str + " "
-    return str
+	if len(str) < i:
+		for j in range(i-len(str)):
+			str = str + " "
+	return str
  
 def get_hostname( ip ):
-    global previous_ip
-    global previous_host
-    if previous_ip == ip:
-        return previous_host
-    else:
-        try:
-            new_host = socket.gethostbyaddr(ip)
-            previous_ip = ip
-            previous_host = new_host[0]
-            return new_host[0]
-        except Exception:
-            new_host = ip
-            previous_ip = ip
-            previous_host = ip
-            return new_host
- 
+	global previous_ip
+	global previous_host
+	if previous_ip == ip:
+		return previous_host
+	else:
+		try:
+			new_host = socket.gethostbyaddr(ip)
+			previous_ip = ip
+			previous_host = new_host[0]
+			return new_host[0]
+		except Exception:
+			new_host = ip
+			previous_ip = ip
+			previous_host = ip
+			return new_host
+
+def get_agent(agent_string):
+
+	return "Firefox"
+
 # READ FILE
 with open(log_path, 'rt') as log:
-    text = log.read();
+	text = log.read()
  
 # 185.31.48.30 - imr [12/Feb/2015:15:42:38 +0100] "GET /rt/rutorrent/plugins/diskspace/action.php?_=1423752158361 HTTP/1.1" 200 329 
 # "https://jane.duckdns.org/rt/rutorrent/" "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0"
@@ -46,9 +49,23 @@ with open(log_path, 'rt') as log:
 for line in text.split("\n"):
 	if len(line) > 5:
 		splitted = line.split("\"")
-		print("First part", splitted[0])
-		print("Request part", splitted[1])
-		print("Code part", splitted[2])
-		print("Referer part", splitted[3])
-		print("Agent part", splitted[5])
-	
+
+		first = splitted[0].split(" ")
+		ip = first[0]
+		user = first[2]
+		date = first[3] + first[4]
+
+		request = splitted[1]
+		resp_code = splitted[2]
+		ref = splitted[3]
+		agent = get_agent(splitted[5])
+
+		print("IP:", ip)
+		print("User:", user)
+		print("date:", date)
+		print("request:", request)
+		print("resp_code:", resp_code)
+		print("referrer:", ref)
+		print("agent:", agent)
+		print()
+
