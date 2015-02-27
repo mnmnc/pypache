56b0abbdb02c060fdb3701beeb235dac88e8d3cc
mnmnc <chooseright+git@gmail.com>
2015-02-27 10:46:58 +0100

    Cleaning up.

--- tools/transformer.py
+++
@@ -1,8 +0,0 @@
-
-class Transformer:
-
-	def adjust_item(self, local_string, i ):
-		if len(local_string) < i:
-			for j in range(i-len(local_string)):
-				local_string = local_string + " "
-		return local_string
