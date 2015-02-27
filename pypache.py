cbf9edf0c9abfb86c25ffb9d37e1e7e8774ad124
mnmnc <mnmnc@users.noreply.github.com>
2015-02-27 12:29:19 +0100

    Backing up to original version 1.0

--- pypache.py
+++ pypache.py
@@ -3,148 +3,123 @@
 # IMPORTS
 import re
 import socket
-
+from colorama import init, Fore, Back, Style
  
 # VARS
-log_path = 'apache.log'
+regex = '([(\d\.)]+) - - \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)"'
+log_path = '/var/log/apache2/access.log'
 previous_ip = " "
 previous_host = " "
  
 # FUNCTIONS
 def adjust_item( str, i ):
-	if len(str) < i:
-		for j in range(i-len(str)):
-			str = str + " "
-	return str
+    if len(str) < i:
+        for j in range(i-len(str)):
+            str = str + " "
+    return str
  
 def get_hostname( ip ):
-	global previous_ip
-	global previous_host
-	if previous_ip == ip:
-		return previous_host
-	else:
-		try:
-			new_host = socket.gethostbyaddr(ip)
-			previous_ip = ip
-			previous_host = new_host[0]
-			return new_host[0]
-		except Exception:
-			new_host = ip
-			previous_ip = ip
-			previous_host = ip
-			return new_host
-
-def get_agent(agent_string):
-	if "Firefox/" in agent_string:
-		return "Firefox"
-	elif "MSIE" in agent_string:
-		return "IE"
-	elif "Seamonkey/" in agent_string:
-		return "Seamonkey"
-	elif "Chrome/" in agent_string and "Chromium" not in agent_string:
-		return "Chrome"
-	elif "Chromium/" in agent_string:
-		return "Chromium"
-	elif "Safari/" in agent_string and "Chrome" not in agent_string and "Chromium" not in agent_string:
-		return "Safari"
-	elif "OPR/" in agent_string or "Opera/" in agent_string:
-		return "Opera"
-	else:
-		return "UNKNWN"
-
-def minimal_print(text):
-
-	# FOR EACH LINE
-	for line in text.split("\n"):
-		if len(line) > 5:
-
-			try:
-
-				# BUILDING DATA
-				splitted = line.split("\"")
-
-				first = splitted[0].split(" ")
-				ip = first[0]
-				user = first[2]
-				date = first[3] + first[4]
-
-				request = splitted[1]
-				resp_code = splitted[2]
-				ref = splitted[3]
-				agent = get_agent(splitted[5])
-
-
-				print(date, end="")
-				print(resp_code, end="")
-				print(agent, end=" ")
-				print(ip, end=" ")
-				print(get_hostname(ip), end=" ")
-				print(user, end=" ")
-				print(ref, end=" ")
-				print(request, end=" ")
-				print()
-
-			except:
-				pass
-
-def print_adjusted(text):
-
-	# FOR EACH LINE
-	for line in text.split("\n"):
-		if len(line) > 5:
-
-			try:
-
-				# BUILDING DATA
-				splitted = line.split("\"")
-
-				first = splitted[0].split(" ")
-				ip = first[0]
-				user = first[2]
-				date = first[3] + first[4]
-
-				request = splitted[1]
-				resp_code = splitted[2]
-				ref = splitted[3]
-				agent = get_agent(splitted[5])
-
-				# ADJUSTING
-				agent = adjust_item(agent, 10)
-				ip = adjust_item(ip, 15)
-				hostname = adjust_item(get_hostname(ip), 30)
-				user = adjust_item(user, 5)
-				ref = adjust_item(ref, 20)
-				request_list = request.split(" ")
-				request_method = request_list[0]
-				request_file = adjust_item(request_list[1], 30)
-				request_protocol = (request_list[2]).split("/")[1]
-
-				print(date, end=" ")
-				print(ip, end=" ")
-				print(hostname, end=" ")
-				print(resp_code.split(" ")[1], end=" ")
-				print(agent, end=" ")
-				print(user, end=" ")
-				print(ref[:20], end=" ")
-				print(request_method[0], end=" ")
-				print(request_protocol, end=" ")
-				print(request_file[:30], end=" ")
-				print()
-
-			except:
-				print("[ERR] Unable to process:", line)
-				pass
-
-
-def main():
-
-	# READ FILE
-	with open(log_path, 'rt') as log:
-		text = log.read()
-
-	# minimal_print(text)
-	#print_adjusted(text)
-
-
-if __name__ == "__main__":
-	main()
+    global previous_ip
+    global previous_host
+    if previous_ip == ip:
+        return previous_host
+    else:
+        try:
+            new_host = socket.gethostbyaddr(ip)
+            previous_ip = ip
+            previous_host = new_host[0]
+            return new_host[0]
+        except Exception:
+            new_host = ip
+            previous_ip = ip
+            previous_host = ip
+            return new_host
+ 
+# READ FILE
+with open(log_path, 'rt') as log:
+    text = log.read();
+ 
+# FOR EACH LINE
+for line in text.split("\n"):
+    if len(line) > 5:
+ 
+        # PARSE LINE AND ADJUST FIELD LENGTH
+        ip      = adjust_item( re.match( regex, line ).group( 1 ), 15 )
+        hostname    = adjust_item( str(get_hostname(ip.strip())), 30 )
+        date        = (re.match( regex, line ).group( 2 )).split(" ")[0]
+        request     = adjust_item( re.match( regex, line ).group( 3 ), 40 )
+        code        = adjust_item( re.match( regex, line ).group( 4 ), 4 )
+        size        = adjust_item( re.match( regex, line ).group( 5 ), 8 )
+        ref     = adjust_item( re.match( regex, line ).group( 6 ), 30 )
+        agent       = adjust_item( re.match( regex, line ).group( 7 ), 3 )
+ 
+        # HTTP 200 OK
+        if code.strip()[0] == "2":
+ 
+            print( date + " " , end="")
+            print( Fore.GREEN + Style.BRIGHT + code[:4] + Fore.RESET + Style.NORMAL, end="" )
+            print( ip[:15] + " " , end="")
+            print( hostname[:30] + " " , end="")
+            print( size[:8] + " " , end="")
+ 
+            # CHECK IF METHOD USED IS GET | POST
+            if request[0] == "G" or request[0] == "P" :
+                print( request[:40] + " " , end="")
+            else:
+                # OTHER METHODS PRINT IN COLOR
+                print( Back.BLACK + Fore.RED + Style.DIM + request[:40] + Fore.RESET + Back.RESET + Style.NORMAL + " ", end="" )
+ 
+            print( ref[:30] + " ", end="")
+            print( agent[:3])
+ 
+        # HTTP 300
+        elif code.strip()[0] == "3":
+            print( date + " " , end="")
+            print( Fore.YELLOW + Style.BRIGHT + code[:4] + Fore.RESET + Style.NORMAL,  end="" )
+            print( ip[:15] + " " , end="")
+            print( hostname[:30] + " " , end="")
+            print( size[:8] + " " , end="")
+            print( request[:40] + " " , end="")
+            print( ref[:30] + " ", end="")
+            print( agent[:3])
+ 
+        # HTTP 400
+        elif code.strip()[0] == "4":
+            print( date + " " , end="")
+            print( Fore.BLUE + Style.BRIGHT + code[:4] + Fore.RESET + Style.NORMAL,  end="" )
+            print( ip[:15] + " " , end="")
+            print( hostname[:30] + " " , end="")
+            print( size[:8] + " " , end="")
+ 
+            # CHECK IF METHOD USED IS GET | POST
+            if request[0] == "G" or request[0] == "P" :
+                print( request[:40] + " " , end="")
+            else:
+                # OTHER METHODS PRINT IN COLOR
+                new_request=Fore.RED + request[:40] + Fore.RESET
+                print( new_request + " " , end="")
+            print( ref[:30] + " ", end="")
+            print( agent[:3])
+ 
+        # HTTP 500
+        elif code.strip()[0] == "5":
+            print( date+ " " , end="")
+            print( Fore.MAGENTA + Style.BRIGHT + code[:4] + Fore.RESET + Style.NORMAL ,  end="" )
+            print( ip[:15] + " " , end="")
+            print( hostname[:30] + " " , end="")
+            print( size[:8] + " " , end="")
+            print( request[:40] + " " , end="")
+            print( ref[:30] + " ", end="")
+            print( agent[:3])
+ 
+        # OTHER
+        else:
+            print( date + " " , end="")
+            print( code[:4],  end="" )
+            print( ip[:15] + " " , end="")
+            print( hostname[:30] + " " , end="")
+            print( size[:8] + " " , end="")
+            print( request[:40] + " " , end="")
+            print( ref[:30] + " ", end="")
+            print( agent[:3])
+            
